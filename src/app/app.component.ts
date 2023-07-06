65d2486e383f07e1e9f6fb8d5055ce6c6ef170da
baskarmadhu <baskarm988@gmail.com>
2023-07-06 10:56:07 +0530

    app module change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component , OnInit} from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'mock-server';
+
+  constructor(private http: HttpClient){}
+
+  ngOnInit(){
+      this.http.get('http://localhost:3000/api/users').subscribe((data) => {
+        console.log(data);
+      })
+  }
 }
